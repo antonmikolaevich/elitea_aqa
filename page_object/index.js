6be49f79a97edd365e37b54fb4d50d4e31274362
antonmikolaevich <57948151+antonmikolaevich@users.noreply.github.com>
2026-04-28 21:22:50 +0200

    Update page_object/index.js

--- page_object/index.js
+++ page_object/index.js
@@ -1,7 +1,9 @@
 const { locator } = require("@qavajs/steps-playwright/po.js");
 const QavaHomePage = require("./QavaHomePage");
+const IntroPage = require("./IntroPage");
 
 module.exports = class App {
   QavaHomePage = locator.as(QavaHomePage);
+  IntroPage = locator.as(IntroPage);
 
 }
