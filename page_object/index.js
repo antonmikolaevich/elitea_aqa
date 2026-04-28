b5c025873eaffffb746dedb90282555e9f227c13
antonmikolaevich <57948151+antonmikolaevich@users.noreply.github.com>
2026-04-28 18:20:51 +0200

    Update page_object/index.js

--- page_object/index.js
+++ page_object/index.js
@@ -5,3 +5,7 @@ module.exports = class App {
   QavaHomePage = locator.as(QavaHomePage);
 
 }
+
+// NOTE: We use our custom POMs in pages/*.page.js and steps/intro.steps.js directly.
+// If you prefer to expose them via page_object, create new wrappers here and map them
+// with locator.as().
